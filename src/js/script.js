eed9f3a1583e8a5b58e0d96b46d9e1e296280ea0
Vladimir Seledkin <vladimyr_seledkin@mail.ru>
2022-01-25 22:45:25 +0300

    Add animations menu

--- src/js/script.js
+++ src/js/script.js
@@ -2,18 +2,22 @@ window.addEventListener("DOMContentLoaded", () => {
   const hamburger = document.querySelector(".hamburger"),
     overlay = document.querySelector(".menu__overlay"),
     menu = document.querySelector(".menu"),
-    closeElem = document.querySelector(".menu__close");
+    closeElem = document.querySelector(".menu__close"),
+    socity = document.querySelector(".socity_panel");
 
   hamburger.addEventListener("click", () => {
     menu.classList.add("active");
+    socity.classList.add("active");
   });
 
   overlay.addEventListener("click", () => {
     menu.classList.remove("active");
+    socity.classList.remove("active");
   });
 
   closeElem.addEventListener("click", () => {
     menu.classList.remove("active");
+    socity.classList.remove("active");
   });
 
   window.addEventListener("keydown", (e) => {
