8e159594f4d427b54b553175c3ed1b9bd007ece0
Vladimir Seledkin <vladimyr_seledkin@mail.ru>
2022-08-31 23:04:04 +0300

    Refictoring

--- gulpfile.js
+++ gulpfile.js
@@ -3,47 +3,63 @@
 const gulp = require("gulp");
 const webpack = require("webpack-stream");
 const browsersync = require("browser-sync");
+//
+const sass        = require('gulp-sass')(require('sass'));
+const cleanCSS = require('gulp-clean-css');
+const autoprefixer = require('gulp-autoprefixer');
+const rename = require("gulp-rename");
+const imagemin = require('gulp-imagemin');
 const htmlmin = require('gulp-htmlmin');
 
 const dist = "./dist/";
 
-gulp.task("copy-html", () => {
-    return gulp.src("./src/*.html")
-                .pipe(gulp.dest(dist))
-                .pipe(browsersync.stream());
+gulp.task('styles', function() {
+    return gulp.src("src/sass/**/*.+(scss|sass)")
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(rename({suffix: '.min', prefix: ''}))
+        .pipe(autoprefixer())
+        .pipe(cleanCSS({compatibility: 'ie8'}))
+        .pipe(gulp.dest("dist/css"))
+        .pipe(browsersync.stream());
 });
 
+gulp.task("copy-html", () => {
+    return gulp.src("./src/*.html")
+        .pipe(htmlmin({ collapseWhitespace: true }))
+        .pipe(gulp.dest(dist))
+        .pipe(browsersync.stream());
+});
 
 gulp.task("build-js", () => {
     return gulp.src("./src/js/main.js")
-                .pipe(webpack({
-                    mode: 'development',
-                    output: {
-                        filename: 'script.js'
-                    },
-                    watch: false,
-                    devtool: "source-map",
-                    module: {
-                        rules: [
-                          {
-                            test: /\.m?js$/,
-                            exclude: /(node_modules|bower_components)/,
-                            use: {
-                              loader: 'babel-loader',
-                              options: {
-                                presets: [['@babel/preset-env', {
-                                    debug: true,
-                                    corejs: 3,
-                                    useBuiltIns: "usage"
-                                }]]
-                              }
-                            }
-                          }
-                        ]
+        .pipe(webpack({
+            mode: 'development',
+            output: {
+                filename: 'script.js'
+            },
+            watch: false,
+            devtool: "source-map",
+            module: {
+                rules: [
+                  {
+                    test: /\.m?js$/,
+                    exclude: /(node_modules|bower_components)/,
+                    use: {
+                      loader: 'babel-loader',
+                      options: {
+                        presets: [['@babel/preset-env', {
+                            debug: true,
+                            corejs: 3,
+                            useBuiltIns: "usage"
+                        }]]
                       }
-                }))
-                .pipe(gulp.dest(dist))
-                .on("end", browsersync.reload);
+                    }
+                  }
+                ]
+              }
+        }))
+        .pipe(gulp.dest(dist))
+        .on("end", browsersync.reload);
 });
 
 gulp.task("copy", () => {
@@ -52,8 +68,14 @@ gulp.task("copy", () => {
                 .on("end", browsersync.reload);
 });
 
+gulp.task('images', function () {
+    return gulp.src("src/img/**/*")
+        .pipe(imagemin())
+        .pipe(gulp.dest("dist/img"))
+        .pipe(browsersync.stream());
+});
+
 gulp.task("watch", () => {
-    
     browsersync.init({
       // для запуска нескольких HTML файлов
         server: {
@@ -69,16 +91,13 @@ gulp.task("watch", () => {
     gulp.watch("./src/*.html", gulp.parallel("copy-html"));
     gulp.watch("./src/**/*.*", gulp.parallel("copy"));
     gulp.watch("./src/js/**/*.js", gulp.parallel("build-js"));
+    gulp.watch("src/img/**/*").on('all', gulp.parallel('images'));
 });
 
 
-gulp.task('html', function () {
-    return gulp.src("src/*.html")
-        .pipe(htmlmin({ collapseWhitespace: true }))
-        .pipe(gulp.dest("dist/"));
-});
 
-gulp.task("build", gulp.parallel("copy-html", "copy", "build-js"));
+gulp.task("build", gulp.parallel("copy-html", "copy", "build-js", 'images'));
+
 
 gulp.task("build-prod-js", () => {
     return gulp.src("./src/js/main.js")
